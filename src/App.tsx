dbe9425032afb0f492203b3bd4fe68723ba4e2b2
nrmistry <nilum@outlook.com>
2023-12-12 16:03:17 +0000

    link ticket to app to get it on

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.scss';
+import TicketCard from "./components/TicketCard/TicketCard"; 
 
 const App = ()=> {
   return (
     <div className ="app">
-      
+      <TicketCard/> 
     </div>
   );
   
