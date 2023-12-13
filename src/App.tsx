461c119f426005b49e38cd1a2fad49b67abcc2af
nrmistry <nilum@outlook.com>
2023-12-13 16:20:45 +0000

    SeachBox Component tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,39 @@ import './App.scss';
 import teams from "./data/teams";
 import TicketBoard from "./containers/TicketBoard/TicketBoard";
 //import Members from "./types/Members";
+import {FormEvent, useState} from "react";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 const App = ()=> {
+
+  const [ searchTerm, setSearchTerm] = useState<string>("");
+  
+
+  const handleSearchInputChange = (event: FormEvent<HTMLInputElement>)  => {
+    setSearchTerm(event.currentTarget.value);
+    console.log('search Term:', event.currentTarget.value)
+  }
+
+  const filteredTeam = teams.filter (
+    member => {
+      const nameMatch = member.name.toLowerCase().includes(searchTerm.toLowerCase());
+      const roleMatch = member.role.toLowerCase().includes(searchTerm.toLowerCase());
+      console.log('Member:', member.name, 'Name Match:', nameMatch, 'Role Match:', roleMatch);
+      return nameMatch || roleMatch;
+    })
+  
+    console.log('Filtered Team', filteredTeam);
+
+
   return (
     <div className ="app">
-      <TicketBoard members={teams} /> 
+      <h1 className="appTitle">Ticket Tracker</h1>
+      <SearchBox
+        label="Search"
+        searchTerm={searchTerm}
+        handleInput={handleSearchInputChange}
+        />
+      <TicketBoard members={filteredTeam} /> 
     </div>
   );
   
